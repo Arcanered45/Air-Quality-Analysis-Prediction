3f670ff635c015462476ca9ba831d1ef99c8a271
Alden C S <146726682+Arcanered45@users.noreply.github.com>
2024-10-22 21:53:02 +0530

    Update prediction.py

--- scripts/prediction.py
+++ scripts/prediction.py
@@ -5,8 +5,8 @@ from sklearn.metrics import mean_squared_error
 
 data = pd.read_csv('../data/cleaned_data.csv')
 
-X = data[['Temperature', 'Humidity', 'WindSpeed']]  # Replace with actual feature columns
-y = data['PM2.5']  # Replace with the actual target variable
+X = data[['Temperature', 'Humidity', 'WindSpeed']]  
+y = data['PM2.5']  
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
