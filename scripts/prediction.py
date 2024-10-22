753fc7ad9bfaddb5ce63cbd0040ef4b4d664db89
Alden C S <146726682+Arcanered45@users.noreply.github.com>
2024-10-22 22:06:42 +0530

    Update prediction.py

--- scripts/prediction.py
+++ scripts/prediction.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-data = pd.read_csv('../data/cleaned_data.csv')
+data = pd.read_csv('cleaned_data.csv')
 
 X = data[['Temperature', 'Humidity', 'WindSpeed']]  
 y = data['PM2.5']  
