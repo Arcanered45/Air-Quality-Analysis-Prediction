4ca23f548c99c108fa2ade36fd422bba7c2f741e
Alden C S <146726682+Arcanered45@users.noreply.github.com>
2024-10-22 21:53:37 +0530

    Update data_cleaning.py

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -7,7 +7,7 @@ raw_data = pd.read_csv('../data/raw_data.csv')
 cleaned_data = raw_data.dropna()
 
 
-# cleaned_data = cleaned_data[cleaned_data['PM2.5'] < threshold_value]
+
 
 
 cleaned_data.to_csv('../data/cleaned_data.csv', index=False)
